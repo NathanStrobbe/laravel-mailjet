8230ab921f303563065f4c06080935dc5d3d6e70
Nathan Strobbe <nathan.strobbe@etu.unice.fr>
2019-12-27 15:13:36 +0100

    Implements attachments to email

--- src/MailjetTransport.php
+++ src/MailjetTransport.php
@@ -17,45 +17,66 @@ class MailjetTransport extends Transport
     public function send(Swift_Mime_Message $message, &$failedRecipients = null)
     {
         $this->beforeSendPerformed($message);
-        
+
         $email = $this->buildEmailData($message);
-        
+
         return $this->client->post(\Mailjet\Resources::$Email, ['body' => $email]);
     }
-    
+
     protected function buildEmailData(Swift_Mime_Message $message)
     {
         $data = [];
-        
+
         foreach ($message->getFrom() as $address => $display) {
             $data['FromEmail'] = $address;
             $data['FromName'] = $display != null ? $display : $address;
             break;
         }
-        
+
         foreach ($message->getTo() as $address => $display) {
             $data['Recipients'][] = ['Email' => $address, 'Name' => $display];
         }
-        
+
         $data['Subject'] = $message->getSubject();
-        
-        if ($message->getContentType() == 'multipart/alternative') {
+
+        if ($message->getContentType() == 'multipart/alternative' || $message->getContentType() == 'multipart/mixed') {
             $data['Html-Part'] = $message->getBody();
             foreach ($message->getChildren() as $child) {
                 if ($child->getContentType() == 'text/plain') {
                     $data['Text-Part'] = $child->getBody();
                 }
             }
-        }
-        elseif ($message->getContentType() == 'text/html') {
+        } elseif ($message->getContentType() == 'text/html') {
             $data['Html-Part'] = $message->getBody();
-        }
-        elseif ($message->getContentType() == 'text/plain') {
+        } elseif ($message->getContentType() == 'text/plain') {
             $data['Text-Part'] = $message->getBody();
         }
-        
-        // TODO: Attachments
-        
+
+        $attachments = $this->getAttachments($message);
+        if (count($attachments) > 0) {
+            $data['Attachments'] = $attachments;
+        }
+
         return $data;
+    }
+
+    /**
+     * @param Swift_Mime_Message $message
+     * @return array
+     */
+    private function getAttachments(Swift_Mime_Message $message)
+    {
+        $attachments = [];
+        foreach ($message->getChildren() as $attachment) {
+            if ($attachment->getContentType() != "application/octet-stream") {
+                continue;
+            }
+            $attachments[] = [
+                'Content-type' => $attachment->getContentType(),
+                'Filename' => $attachment->getFilename(),
+                'content' => base64_encode($attachment->getBody()),
+            ];
+        }
+        return $attachments;
     }
 }
